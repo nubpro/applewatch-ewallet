736166a45df8bb249d8a50c85385a0484b5b2b89
Chai Woon Khai <chai1998@live.com>
2021-12-26 22:56:17 +0800

    dev

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      sans: ["Open Sans", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        primary: {
+          400: "#FFE419",
+          850: "#482504",
+          900: "#381C01",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
