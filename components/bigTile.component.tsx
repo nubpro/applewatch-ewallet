3459cac2beb7772d559ed1c06186c7e82ec92b93
Chai Woon Khai <chai1998@live.com>
2022-06-01 14:46:08 +0800

    chore: change tiles color

--- components/bigTile.component.tsx
+++ components/bigTile.component.tsx
@@ -13,7 +13,7 @@ const BigTile = ({
 }) => {
   return (
     <div className="container mx-auto">
-      <div className="bg-white shadow-xl shadow-secondary-400/5 hover:shadow-secondary-400/10 transition-shadow sm:rounded-2xl grid grid-cols-1 md:grid-cols-3 py-10 px-8 md:px-12 gap-y-12 md:gap-x-5">
+      <div className="bg-primary-400 shadow-xl shadow-primary-400/10 hover:shadow-primary-400/40 transition-shadow sm:rounded-2xl grid grid-cols-1 md:grid-cols-3 py-10 px-8 md:px-12 gap-y-12 md:gap-x-5">
         <div className="col-span-2">
           <div className="text-3xl text-secondary-400 font-extrabold">
             {title}
@@ -21,7 +21,7 @@ const BigTile = ({
           <div className="pt-3 md:w-5/6">{description}</div>
         </div>
 
-        <div className="w-52 mx-auto">
+        <div className="w-44 mx-auto">
           <Image src={imageSrc} alt={imageAlt} />
         </div>
       </div>
