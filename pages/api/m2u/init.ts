80eb1b81b4abfc2a73e8e0fd7dffe861aec99820
Chai Woon Khai <chai1998@live.com>
2023-08-26 00:51:53 +0800

    chore: update m2u to 10.0

--- pages/api/m2u/init.ts
+++ pages/api/m2u/init.ts
@@ -16,7 +16,7 @@ class ErrorResponse extends Error {
   }
 }
 
-export const CURRENT_APPVERSION: string = "9.9";
+export const CURRENT_APPVERSION: string = "10.0";
 
 export async function requestInit(appVersion = CURRENT_APPVERSION) {
   // Header params
