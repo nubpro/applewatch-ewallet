5a0d0e9f29ee121202ad232471e181d944815df2
Chai Woon Khai <chai1998@live.com>
2023-09-19 23:50:22 +0800

    fix: svg error

--- pages/index.tsx
+++ pages/index.tsx
@@ -63,9 +63,9 @@ const Home: NextPage = () => {
                   >
                     <path
                       stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth-width="2"
                       d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
                     />
                   </svg>
